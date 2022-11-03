4b1a8db1e33e922d4c6fbb86be9b2dddf911edc7
Christian <cristianbach789@gmail.com>
2022-11-03 09:19:44 -0500

    ejercicio 1 sem15

---
+++ JAVASCRIPT/SEMANA 15/JS/DIGITURNO.js
@@ -0,0 +1,31 @@
+function prefer(){
+    let num=1;
+    let res=true;
+    while (num<=99 && res==true){
+        nomuser=prompt("INGRESE SU NOMBRE DE USUARIO");
+        if(nomuser!==null){
+            var n=num.toString().padStart(2,'0');
+                res=confirm("ESTIMADO USUARIO:"+" "+" "+nomuser+"\n"+"SU TURNO ES:"+" "+" "+"E"+n+"\n"+"ESPERE SU LLAMADO...");
+                num++;
+                }else{
+                    alert("ERROR EN TURNO …");
+                    break;
+                }
+            }
+        }
+        function user(){
+        let num=1;
+        let res=true;
+        while (num<=99 && res==true){
+            nomuser=prompt("INGRESE SU NOMBRE DE USUARIO");
+            if (nomuser!==null){
+                var n=num.toString().padStart(2,'0');
+                res=confirm("ESTIMADO USUARIO:"+" "+" "+nomuser+"\n"+"SU TURNO ES:"+" "+"A"+n+"\n"+"ESPERE SU LLAMADO...");
+                num++;
+                }else{
+                    alert("ERROR EN TURNO…");
+                    break;
+
+        }
+    }
+}
